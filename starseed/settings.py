2074bc718e0deb65be8aca916b6affb384675159
fredfalcon <7475382+fredfalcon@users.noreply.github.com>
2021-02-27 20:41:47 -0500

    update staticfiles

--- starseed/settings.py
+++ starseed/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'starseed',
     'taggit',
@@ -45,6 +46,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
