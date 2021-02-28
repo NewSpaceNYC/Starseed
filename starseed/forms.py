4ab98e4b7d9296aaaffb37cea0f2fd871a8548b7
fredfalcon <7475382+fredfalcon@users.noreply.github.com>
2021-02-27 21:37:06 -0500

    add sendgrid api

--- starseed/forms.py
+++ starseed/forms.py
@@ -3,21 +3,40 @@ from captcha.fields import CaptchaField
 from starseed.models import Project
 from django.core.mail import EmailMessage
 from django.conf import settings
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 class CaptchaTestModelForm(forms.ModelForm):
     captcha = CaptchaField()
 
     def send_email(self):
-        email = EmailMessage(
-            'Application from ' + self.cleaned_data["name"],
-            'Name: ' + self.cleaned_data["person"] + \
-            'Email: ' + self.cleaned_data["email"] + \
-            'IdeaName: ' + self.cleaned_data["name"] + \
-            'Description: ' + self.cleaned_data["description"],
-            settings.SERVER_EMAIL,
-            [settings.SERVER_EMAIL],
-        )
-        email.send(fail_silently=False)
+        # email = EmailMessage(
+        #     'Application from ' + self.cleaned_data["name"],
+        #     'Name: ' + self.cleaned_data["person"] + \
+        #     'Email: ' + self.cleaned_data["email"] + \
+        #     'IdeaName: ' + self.cleaned_data["name"] + \
+        #     'Description: ' + self.cleaned_data["description"],
+        #     settings.SERVER_EMAIL,
+        #     [settings.SERVER_EMAIL],
+        # )
+        # email.send(fail_silently=False)
+
+        message = Mail(
+            from_email=settings.SERVER_EMAIL,
+            to_emails=settings.SERVER_EMAIL,
+            subject='Application from ' + self.cleaned_data["name"],
+            html_content='Name: ' + self.cleaned_data["person"] + 'Email: ' + self.cleaned_data["email"] + 'IdeaName: ' + self.cleaned_data["name"] + 'Description: ' + self.cleaned_data["description"])
+        print(message)
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY', ''))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
 
     class Meta:
         fields =  '__all__'
