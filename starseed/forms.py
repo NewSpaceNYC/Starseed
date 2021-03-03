3306af290b6e982d8fd8089a3945373bd288c633
fredfalcon <7475382+fredfalcon@users.noreply.github.com>
2021-03-02 19:34:54 -0500

    fix emails

--- starseed/forms.py
+++ starseed/forms.py
@@ -20,21 +20,29 @@ class CaptchaTestModelForm(forms.ModelForm):
 
         message2 = Mail(
             from_email=settings.SERVER_EMAIL,
-            to_emails=self.cleaned_data["email1"],
+            to_emails=To(self.cleaned_data["email1"],''),
             subject=self.cleaned_data["person"] + ' suggests you apply for the Starseed - pico innovation fund',
             html_content='Hi There!: <br>' + self.cleaned_data["person"] + ' suggested you apply for the Starseed pico innovation fund.<br><br>Apply today at https://starseed.fund ')
 
-        message2 = Mail(
+        message3 = Mail(
             from_email=settings.SERVER_EMAIL,
-            to_emails=self.cleaned_data["email1"],
+            to_emails=To(self.cleaned_data["email1"],''),
             subject=self.cleaned_data["person"] + ' suggests you apply for the Starseed - pico innovation fund',
             html_content='Hi There!: <br>' + self.cleaned_data["person"] + ' suggested you apply for the Starseed pico innovation fund.<br><br>Apply today at https://starseed.fund ')
-        
+
+        message4 = Mail(
+            from_email=settings.SERVER_EMAIL,
+            to_emails=To(self.cleaned_data["email"],self.cleaned_data["person"]),
+            subject='Starseed: Thank you for your application ',
+            html_content='Summary: <br> Name: ' + self.cleaned_data["person"] + '<br>Email: ' + self.cleaned_data["email"] + '<br>IdeaName: ' + self.cleaned_data["name"] + '<br>Equity: ' + self.cleaned_data["equity"] + '<br>Description: ' + self.cleaned_data["description"])
+
+
         try:
             sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY', ''))
             response = sg.send(message1)
             response = sg.send(message2)
             response = sg.send(message3)
+            response = sg.send(message4)
         except Exception as e:
             print(e)
 
