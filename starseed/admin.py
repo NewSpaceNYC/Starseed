85c2b72ca0f2654d6b1a9b51bd90b13672b5052c
fredfalcon <7475382+fredfalcon@users.noreply.github.com>
2021-03-05 11:14:31 -0500

    commented unused URLS for now and show sessions to get an idea of traffic

--- starseed/admin.py
+++ starseed/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from starseed.models import *
-
+import pprint
 from django.contrib.auth.models import User
-
-
+from django.contrib.sessions.models import Session
+from django.utils.html import mark_safe
 admin.site.register(Person)
 admin.site.register(Organization)
 admin.site.register(Connection)
@@ -14,3 +14,13 @@ class ProjectAdmin(admin.ModelAdmin):
 
 admin.site.register(Project, ProjectAdmin)
 
+
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return mark_safe(pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n'))
+    _session_data.allow_tags=True
+    list_display = ['session_key', '_session_data', 'expire_date']
+    readonly_fields = ['_session_data']
+    exclude = ['session_data']
+    date_hierarchy='expire_date'
+admin.site.register(Session, SessionAdmin)
